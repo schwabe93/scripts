db7437e95a48ab1df35d7eba1b8c85b0fd2e383a
Alexander Gemsa <alexander.gemsa@gmail.com>
2019-07-06 11:44:21 +0530

    Add myself

--- personal-setup/deb-setup.sh
+++ personal-setup/deb-setup.sh
@@ -1,9 +1,8 @@
 source ~/scripts/setup/hub.sh
-git config --global user.name "Akhil Narang"
-git config --global user.email "akhilnarang.1999@gmail.com"
+git config --global user.name "Alexander Gemsa"
+git config --global user.email "alexander.gemsa@gmail.com"
 git config --global credential.helper "cache --timeout=7200"
 echo "" >> ~/.bashrc
 echo "source ~/scripts/functions" >> ~/.bashrc
 echo "onLogin" >> ~/.bashrc
 sudo apt install figlet fortune byobu mosh -y
-
